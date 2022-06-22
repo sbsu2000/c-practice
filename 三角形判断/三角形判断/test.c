75f9a1f62c9ff966bac6a56ae6afbbe8eba2f534
卷啊卷 <98833727+sbsu2000@users.noreply.github.com>
2022-06-22 23:09:44 +0800

    Add files via upload

---
+++ 三角形判断/三角形判断/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    while (scanf("%d %d %d", &a, &b, &c) != EOF)
+    {
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
+                printf("Isosceles triangle!\n");
+
+            else if (a == b && b == c)
+                printf("Equilateral triangle!\n");
+            else
+                printf("Ordinary triangle!\n");
+        }
+        else
+            printf("Not a triangle!\n");
+    }
+
+    return 0;
+}
