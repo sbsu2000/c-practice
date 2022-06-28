a14f640295ef6254979a06e1364d20e0f387f058
卷啊卷 <98833727+sbsu2000@users.noreply.github.com>
2022-06-28 23:44:06 +0800

    Add files via upload

---
+++ 说祝福语/说祝福语/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i;
+    int j;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        printf("Happy new year!Good luck!\n");
+    }
+    return 0;
+}
