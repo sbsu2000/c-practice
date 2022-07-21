2f786d1a055ca4ffda3557c76c78bf15af007b0c
卷啊卷 <98833727+sbsu2000@users.noreply.github.com>
2022-07-21 22:12:05 +0800

    Add files via upload

---
+++ 翻转金字塔/翻转金字塔/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i, j;
+    while (scanf("%d", &n) != EOF)
+    {
+        for (i = n; i > 0; i--)
+        {
+            for (j = n; j > 0; j--)
+            {
+                if (j > i)
+                    printf(" ");
+                else
+                    printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
