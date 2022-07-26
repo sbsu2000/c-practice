7dc50e7ef52ad3b1a047069e0f1d25e2d737a6ed
卷啊卷 <98833727+sbsu2000@users.noreply.github.com>
2022-07-26 22:47:47 +0800

    Add files via upload

---
+++ 打印正斜线/打印正斜线/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    int i, j;
+    while (scanf("%d", &n) != EOF)
+    {
+        for (i = n; i > 0; i--)
+        {
+            for (j = 1; j <= n; j++)
+            {
+                if (j == i)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
